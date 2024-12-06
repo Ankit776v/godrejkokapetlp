eec09e8190eae003b42f3a0d5dd3e37277ff1cd7
Ankit776v <122219504+Ankit776v@users.noreply.github.com>
2024-12-06 13:27:47 +0530

    Add files via upload

--- submit.php
+++ submit.php
@@ -1,56 +1,53 @@
 <?php
+//Import PHPMailer classes into the global namespace
+//These must be at the top of your script, not inside a function
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 if(isset($_POST['submit'])){
-$name = $_POST['name'];
-$mobile = $_POST['mobile'];
-$email = $_POST['email'];
-
-
-   
-//email
-$to = "proptimesbangalore@gmail.com,Marketing@proptimes.org,hussainproptimes@gmail.com";
-$subject = "Enquiry From To Sobha Hartland II ";
-
-$htmlContent = '<html>
-<body>
-<div style="background:#eceff0; padding:30px; font-family:Verdana, Geneva, sans-serif; font-size:13px;">
-<div style="background:#fff; border:1px solid#ccc; padding:25px;">
-
-
-
-<br><br>
-You have recived new enquiry from Sobha Hartland II  <br><br>
-
-Details are:<br><br>
-Name: '.$_POST['Name'].'<br>
-County code: '.$_POST['County-code'].'<br>
-Number: '.$_POST['Number'].'<br>
-Email:  '.$_POST['Email'].'<br>
-Project:  '.$_POST['Project'].'<br>
-Location:  '.$_POST['Location'].'<br>
-Ip:  '.$_POST['Ip'].'<br>
-
-
-<br><br>
-Team<br>
-<strong>To Sobha Hartland II </strong><br>
-</div>
-</div>
-</body>
-</html>';
-
-// Set content-type header for sending HTML email
-$headers = "MIME-Version: 1.0" . "\r\n";
-$headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
-
-// Additional headers
-$headers .= 'From:mickeysingh2017@gmail.com'  . "\r\n";
-
-
-// Send email
-mail($to,$subject,$htmlContent,$headers);
-  //end email
-    header('Location: thanks.php');
+    $Name = htmlspecialchars($_POST['Name']);
+    $Email = htmlspecialchars($_POST['Email']);
+    $Number = htmlspecialchars($_POST['Number']);
+    $Location = htmlspecialchars($_POST['Location']);  
+    //Load Composer's autoloader
+    require 'PHPMailer/Exception.php';
+    require 'PHPMailer/PHPMailer.php';
+    require 'PHPMailer/SMTP.php';
+    
+    //Create an instance; passing `true` enables exceptions
+    $mail = new PHPMailer(true);
+    
+    try {
+        //Server settings                  //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = 'ankit786v@gmail.com';                     //SMTP username
+        $mail->Password   = 'jehy ifnj owdn mohy';                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+        
+        //Recipients
+        $mail->setFrom('ankit786v@gmail.com', 'Contact Form');
+        $mail->addAddress('av310437@gmail.com', 'host');     //Add a recipient
+        
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'New Contact Form Submission';
+        $mail->Body    = "
+            <h3>Contact Form Details:</h3>
+            <p><strong>Name:</strong> $Name</p>
+            <p><strong>Email:</strong> $Email</p>
+            <p><strong>Phone Number:</strong> $Number</p>
+            <p><strong>Location:</strong> $Location</p>
+        ";
+        
+        $mail->send();
+        header('Location: thanks.php');
+        exit; 
+        // echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
-?>
-
